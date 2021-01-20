519d6fa6616e934194b6c89ea12b217b4703a573
Repsej2003 <jr@lillebror.it>
2021-01-20 16:31:40 +0100

    Changed conntrack graph layout

--- includes/html/graphs/application/conntrack_connections.inc.php
+++ includes/html/graphs/application/conntrack_connections.inc.php
@@ -7,19 +7,10 @@ require 'includes/html/graphs/common.inc.php';
 $rrd_filename = rrd_name($device['hostname'], ['app', 'conntrack', $app['app_id']]);
 
 $rrd_options .= ' DEF:Connctions=' . $rrd_filename . ':Connctions:AVERAGE';
-$rrd_options .= " 'COMMENT:Connctions        Cur      Min     Max     Avg\\n'";
+$rrd_options .= " 'COMMENT:Connctions              Cur      Max     Avg\\n'";
 
-$rrd_options .= ' LINE1.25:Connctions#36393d:Connctions';
-
-$rrd_options .= ' GPRINT:Connctions:LAST:%14.2lf  GPRINT:Connctions:AVERAGE:%6.2lf';
-$rrd_options .= " GPRINT:Connctions:MAX:%6.2lf  'GPRINT:Connctions:AVERAGE:%6.2lf\\n'";
-
-/*require 'includes/html/graphs/common.inc.php';
-
-$rrd_filename = rrd_name($device['hostname'], ['app', 'conntrack', $app['app_id']]);
-
-$rrd_options .= ' DEF:Connctions=' . $rrd_filename . ':Connctions:AVERAGE';
-$rrd_options .= " 'COMMENT:                        Now      Min     Max     Avg\\n'";
-$rrd_options .= ' LINE1.25:Connctions#36393d:Connctions';
-$rrd_options .= ' GPRINT:Connctions:LAST:%14.2lf  GPRINT:Connctions:AVERAGE:%6.2lf';
+$rrd_options .= ' LINE1.25:Connctions#006400:Connctions';
+$rrd_options .= ' AREA:Connctions#98FB98';
+$rrd_options .= ' --lower-limit=-1';
+$rrd_options .= ' GPRINT:Connctions:LAST:%14.2lf  ';
 $rrd_options .= " GPRINT:Connctions:MAX:%6.2lf  'GPRINT:Connctions:AVERAGE:%6.2lf\\n'";
